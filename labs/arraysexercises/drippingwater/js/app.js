fdc8a99127d76b276627e71cae9d7489c75c4cc8
elissa-murphy <elrmurph@iu.edu>
2021-09-22 21:22:02 -0400

    updated lab 4

--- labs/arraysexercises/drippingwater/js/app.js
+++ labs/arraysexercises/drippingwater/js/app.js
@@ -1,5 +1,8 @@
-var circle_coords=[]; //to store y coordinates of the circles
-var frames_count=0; //to find every 10th frame
+//store y coordinates
+var yCoords=[]; 
+//variable to find every 10th frame 
+var frameCount=0; 
+//variable for circle diameter 
 var diameter=30; 
 
 function setup() {
@@ -9,26 +12,27 @@ createCanvas(400, 400);
 function draw() {
 background(127); 
 
-for(let i = 0; i < circle_coords.length; i++){
+for(let i = 0; i < yCoords.length; i++){
     //using width/2 as x coordinate and circle_coords[i] as y coordinate
-    ellipse(width/2,circle_coords[i],diameter,diameter);
+    ellipse(width/2,yCoords[i],diameter,diameter);
     //moving coordinate 5 pixels down for next iteration
-    circle_coords[i]=circle_coords[i]+5;
+    yCoords[i]=yCoords[i]+5;
 }
 
-//advancing frames count
-frames_count=frames_count+1;
-//if count is 10
+//Add increasing frame count
+frameCount=frameCount+1;
 
-if(frames_count==10){
+
+//if count is 10
+if(frameCount==10){
     //resetting count
-    frames_count=0;
+    frameCount=0;
     //adding a new value to the circle_coords which is the y coordinate
     //of top of the window, which is 0
-    circle_coords.push(0);
+    yCoords.push(0);
 
-    if(circle_coords.length>100){
-      circle_coords.shift();
+    if(yCoords.length>100){
+      yCoords.shift();
     }
 }
 }
--- labs/arraysexercises/snake/js/app.js
+++ labs/arraysexercises/snake/js/app.js
@@ -1,4 +1,6 @@
+//circle radius, can be changed later if needed 
 var radius = 30
+//placeholders for x and y positions
 var x = [0,0,0,0,0,0,0,0,0,0,0];
 var y = [0,0,0,0,0,0,0,0,0,0,0];
 
@@ -7,14 +9,18 @@ function setup (){
 }
 
 function draw () {
+//light grey background
    background(150);
-   
+
+//shifts x positions into array in order to save last ten positions of the mouse
     x.shift();
     x.push(mouseX);
-   
+    
+//shifts y positions into array  in order to save last ten positions of the mouse 
     y.shift();
     y.push(mouseY);
 
+//for loop that draws 11 circles at mouse position
     for (let i = 0; i < 10;i++) {
         ellipse(x[i],y[i],radius,radius);
     }
