2654ab1a4a04b78bbb18e08c8eb253da7a4a2fef
elissa-murphy <elrmurph@iu.edu>
2021-10-27 23:24:22 -0400

    Update app.js

--- labs/user_input_exercises/tip_calculator/js/app.js
+++ labs/user_input_exercises/tip_calculator/js/app.js
@@ -3,9 +3,10 @@ let dvResult = document.getElementById("dvResult");
 
 
 
+
 function doThing() {
     //logs tip and total out to console
-    console.log ("Tip: $" + intTxt.value * 0.2 + " " + "Total: $" + Math.round(intTxt.value * 1.20));
+    console.log ("Tip: $" + intTxt.value * 0.20 + " " + "Total: $" + Math.round(intTxt.value * 1.20));
 
     //clears input field after user clicks button
     intTxt.value = "";
